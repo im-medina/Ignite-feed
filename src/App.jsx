08da41edfa7106068da2a78e7922d7dc91da8e25
Medina <medinnajr@gmail.com>
2022-09-06 00:33:20 -0300

    sidebar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { Header } from './components/header'
 import {Post} from './Post'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
 import './global.css'
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
       <Post 
         author="Medina" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad enim excepturi est amet doloribus accusantium fugiat iusto reprehenderit eaque architecto nemo, facilis distinctio quisquam debitis numquam eos quae unde tenetur?"
@@ -14,7 +21,8 @@ export function App() {
         author="Muzzy"
         content="Eu amo a Roberta"
       />
-      
+      </main>
+      </div>
     </div>
   )
 }
